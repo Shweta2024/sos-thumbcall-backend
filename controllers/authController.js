debdb6e1213fdebfd6e44e9b63c4d828973653e4
Shweta Bhagat <bhagatshweta0216@gmail.com>
2024-05-30 13:41:07 +0530

    add register/login logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,64 @@
-const registerUser = (req, res) => {
-    console.log(req.body)
+const User = require('../models/userModel')
+const bcrypt = require('bcrypt')
+
+
+// function to check if user exists 
+const userExists = async(userEmail) => {
+    const user = await User.findOne({ email: userEmail })
+    return user
 }
 
-const loginUser = (req, res) => {
-    console.log(req.body)
+
+const registerUser = async(req, res) => {
+    try {
+        const { role, name, email, phoneNo, password } = req.body
+        const isUserPresent = await userExists(email)
+        
+        // check if user already exists
+        if (isUserPresent) {
+            res.status(400).send('message: user already exists!')
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+        
+        const user = new User({
+            role,
+            name,
+            email,
+            phoneNo,
+            password: hashedPassword
+        })
+
+        await user.save()
+        res.status(200).send({
+            "id": user._id,
+            "name": user.name
+        })
+    }
+    catch (error) {
+        res.status(400).send(error)
+    }
+    
 }
+
+
+const loginUser = async(req, res) => {
+    const { email, password } = req.body
+    const user = await userExists(email)
+
+    if (!user) {
+        res.status(400).send('message: user not found!')
+    }
+
+    const verifyUser = await bcrypt.compare(password, user.password)
+    console.log(verifyUser)
+    if (!verifyUser) {
+        res.status(400).send('message: incorrect password!')
+    }
+
+    res.status(200).send(`${user.name} logged in!`)
+}
+
 
 module.exports = {
     registerUser,
